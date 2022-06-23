775573f2af677b543f945783f2bfb6d874830a20
Ian Clark <ianclark226@gmail.com>
2022-06-23 13:53:44 -0400

    users get and post request

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 
 
 User = get_user_model()
@@ -7,4 +9,34 @@ User = get_user_model()
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('first_name', 'last_name', 'email', 'password')
+
+    def validate(self, data):
+        user = User(**data)
+        password = data.get('password')
+        try:
+            validate_password(password, user)
+
+        except exceptions.ValidationError as e:
+            serializers_errors = serializers.as_serializer_error(e)
+            raise exceptions.ValidationError(
+                {'password':serializers_errors['non_field_errors']}
+            )
+
+        return data
+
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+        )
+
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,22 +1,23 @@
-from rest_framework import APIView
-from rest_framework import permissions
-from rest_framework.response import Response, status
-from django.contrib.auth import get_user_model
-User = get_user_model()
-from .serializers import UserCreateSerializer
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+
+from .serializers import UserCreateSerializer, UserSerializer
 
 
 class RegisterView(APIView):
 
     def post(self, request):
         data = request.data
-        first_name = data['first_name']
-        last_name = data['last_name']
-        email = data['email']
-        password = data['password']
 
-        user = User.objects.create_user(first_name, last_name, email, password)
-        user = UserCreateSerializer(user)
+        
+        serializer = UserCreateSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user = serializer.create(serializer.validated_data)
+        user = UserSerializer(user)
 
         return Response(user.data, status=status.HTTP_201_CREATED)
 
@@ -24,7 +25,10 @@ class RetrieveUserView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        pass
+        user = request.user
+        user = UserSerializer(user)
+
+        return Response(user.data, status=status.HTTP_200_OK)
 
 
 
