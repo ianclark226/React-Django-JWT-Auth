2b8e1dac591a867183f1bf84f36dee769d47ffa0
Ian Clark <ianclark226@gmail.com>
2022-10-19 11:14:54 -0400

    Toolkit part 1

--- frontend/client/src/pages/RegisterPage.js
+++ frontend/client/src/pages/RegisterPage.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from 'components/Layout';
+import { Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 const RegisterPage = () => {
+  const { registered } = useSelector(state => state.user)
+
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: ''
+  });
+
+  const { first_name, last_name, email, password } = formData;
+
+  if(registered) {
+    return <Navigate to="/login" />
+  }
   return (
     <Layout title="Auth Site | Register" content="Register page">
-      <h1>RegisterPage</h1>
+      <h1>Register for an account</h1>
+      <form>
+        <div>
+          <label htmlFor='first_name'>First Name</label>
+          <input 
+          type="text"
+          name="first_name"
+
+          />
+        </div>
+      </form>
     </Layout>
   )
 }
--- frontend/client/src/store.js
+++ frontend/client/src/store.js
@@ -4,6 +4,7 @@ import userReducer from 'features/user';
 export const store = configureStore({
     reducer: {
         user: userReducer,
-    }
+    },
+    devTools: process.env.NODE_ENV !== 'production',
 })
 
